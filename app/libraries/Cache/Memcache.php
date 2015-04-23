985132758b466f74835faf0d101c47ce947aaec3
RakeshShrestha <rakesh.shrestha@gmail.com>
2015-04-23 15:34:55 +0545

    now we have memcached class also

--- app/libraries/Cache/Memcache.php
+++ app/libraries/Cache/Memcache.php
@@ -2,21 +2,21 @@
 
 final class Cache_Memcache {
 
-    private $_memcached = null;
+    private $_memcache = null;
     private $_memcache_enabled = null;
     private $_lkeydata = array();
 
     public function __construct() {
         $_memcache_enabled = extension_loaded('memcache');
         if ($_memcache_enabled) {
-            $this->_memcached = new Memcache();
-            $this->_memcached->addserver(key(unserialize(MC_SERVER)), current(unserialize(MC_SERVER)));
+            $this->_memcache = new Memcache();
+            $this->_memcache->addserver(key(unserialize(MC_SERVER)), current(unserialize(MC_SERVER)));
         }
     }
 
     public function set($key, $data, $ttl = 3600) {
         if ($_memcache_enabled) {
-            return $this->_memcached->set(sha1($key), array($data, time(), $ttl), 0, $ttl);
+            return $this->_memcache->set(sha1($key), array($data, time(), $ttl), 0, $ttl);
         }
     }
 
@@ -26,7 +26,7 @@ final class Cache_Memcache {
 
     public function valid($key) {
         if ($_memcache_enabled) {
-            $data = $this->_memcached->get(sha1($key));
+            $data = $this->_memcache->get(sha1($key));
             if ($data) {
                 $this->_lkeydata[sha1($key)] = (is_array($data)) ? $data[0] : false;
                 return true;
---
+++ app/libraries/Cache/Memcached.php
@@ -0,0 +1,38 @@
+<?php
+
+final class Cache_Memcached {
+
+    private $_memcached = null;
+    private $_memcached_enabled = null;
+    private $_lkeydata = array();
+
+    public function __construct() {
+        $_memcached_enabled = extension_loaded('memcached');
+        if ($_memcached_enabled) {
+            $this->_memcached = new Memcached();
+            $this->_memcached->addserver(key(unserialize(MC_SERVER)), current(unserialize(MC_SERVER)));
+        }
+    }
+
+    public function set($key, $data, $ttl = 3600) {
+        if ($_memcached_enabled) {
+            return $this->_memcached->set(sha1($key), array($data, time(), $ttl), 0, $ttl);
+        }
+    }
+
+    public function get($key) {
+        return isset($this->_lkeydata[sha1($key)]) ? $this->_lkeydata[sha1($key)] : null;
+    }
+
+    public function valid($key) {
+        if ($_memcached_enabled) {
+            $data = $this->_memcached->get(sha1($key));
+            if ($data) {
+                $this->_lkeydata[sha1($key)] = (is_array($data)) ? $data[0] : false;
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
