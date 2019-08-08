66ce453189ed88cd82a6616559a4abf3fa11b01e
Rakesh Shrestha <rakesh.shrestha@gmail.com>
2019-08-08 11:10:19 +0545

    update session.php

--- app/libraries/Session.php
+++ app/libraries/Session.php
@@ -1,1 +1,17 @@
-<?phpfinal class Session {    private static $_context = null;    public static function getContext($sesstype) {        if (self::$_context === null) {            $classname = 'Session_' . $sesstype;            self::$_context = new $classname;        }        return self::$_context;    }}
+<?php
+
+final class Session {
+
+    private static $_context = null;
+
+    public static function getContext($sesstype) {
+        if (self::$_context === null) {
+            $classname = 'Session_' . $sesstype;
+            self::$_context = new $classname;
+        }
+
+        return self::$_context;
+    }
+
+}
+
