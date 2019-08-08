e89e3bf7897890ff7fa012aaaeb233e06565e0ed
Rakesh Shrestha <rakesh.shrestha@gmail.com>
2019-08-08 11:13:48 +0545

    update controller

--- app/controllers/controller.php
+++ app/controllers/controller.php
@@ -1,1 +1,14 @@
-<?phpabstract class cController {    public $req = null;    public $res = null;    public function __construct() {        $this->req = req();        $this->res = res();    }}
+<?php
+
+abstract class cController {
+
+    public $req = null;
+    public $res = null;
+
+    public function __construct() {
+        $this->req = req();
+        $this->res = res();
+    }
+
+}
+
