39581caa7774565e39934878f6ad2a2cc92ca380
Rakesh Shrestha <rakesh.shrestha@gmail.com>
2019-08-08 11:00:13 +0545

    update line wrap
    update line wrap

--- index.php
+++ index.php
@@ -1,1 +1,32 @@
-<?phpdefine('APP_DIR', realpath(dirname(__FILE__)) . '/app/');require_once APP_DIR . 'config.php';if (DEBUG) {    error_reporting(E_ALL);} else {    error_reporting(0);}require_once APP_DIR . 'coreclasses.php';date_default_timezone_set(SYSTEM_TIMEZONE);$request = Request::getContext();$response = Response::getContext();try {    Application::run($request, $response);} catch (Exception $e) {    header("Content-type: text/html");    $data['message'] = DEBUG ? $e : '';    $data['layout'] = false;    $response->display($data, 'errors/exception');    exit;}
+<?php
+
+define('APP_DIR', realpath(dirname(__FILE__)) . '/app/');
+
+require_once APP_DIR . 'config.php';
+
+if (DEBUG) {
+    error_reporting(E_ALL);
+} else {
+    error_reporting(0);
+}
+
+require_once APP_DIR . 'coreclasses.php';
+
+date_default_timezone_set(SYSTEM_TIMEZONE);
+
+$request = Request::getContext();
+$response = Response::getContext();
+
+try {
+    Application::run($request, $response);
+} catch (Exception $e) {
+    header("Content-type: text/html");
+
+    $data['message'] = DEBUG ? $e : '';
+
+    $data['layout'] = false;
+
+    $response->display($data, 'errors/exception');
+    exit;
+}
+
