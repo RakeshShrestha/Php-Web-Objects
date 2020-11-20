8d9bc7d580400e096f4a5e5e0b4c21d2af046e19
RAKESHLAPTOP\Rakesh <rakesh.shrestha@gmail.com>
2020-11-20 05:50:59 +0545

    firebase cloud message class added

--- admin/app/config/myconfig.php
+++ admin/app/config/myconfig.php
@@ -1,21 +1,22 @@
 <?php
 /**
-# Copyright Rakesh Shrestha (rakesh.shrestha@gmail.com)
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are
-# met:
-#
-# Redistributions must retain the above copyright notice.
-*/
-
+ # Copyright Rakesh Shrestha (rakesh.shrestha@gmail.com)
+ # All rights reserved.
+ #
+ # Redistribution and use in source and binary forms, with or without
+ # modification, are permitted provided that the following conditions are
+ # met:
+ #
+ # Redistributions must retain the above copyright notice.
+ */
 define('SITE_TITLE', 'PWO');
 define('PATH_PREFIX', serialize(array(
     'dashboard',
     'manage',
     'ajax'
 )));
+
+define('FIREBASE_API_KEY', '');
 
 define('SYSTEM_EMAIL', '');
 define('SYSTEM_EMAIL2', '');
--- admin/app/libraries/Csv.php
+++ admin/app/libraries/Csv.php
@@ -1,15 +1,15 @@
 <?php
-/**
-# Copyright Rakesh Shrestha (rakesh.shrestha@gmail.com)
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are
-# met:
-#
-# Redistributions must retain the above copyright notice.
-*/
 
+/**
+ # Copyright Rakesh Shrestha (rakesh.shrestha@gmail.com)
+ # All rights reserved.
+ #
+ # Redistribution and use in source and binary forms, with or without
+ # modification, are permitted provided that the following conditions are
+ # met:
+ #
+ # Redistributions must retain the above copyright notice.
+ */
 final class Csv
 {
 
